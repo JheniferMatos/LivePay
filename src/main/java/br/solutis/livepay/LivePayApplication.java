9fb2e84086afd79a33df39290792e2edf4a709a4
Jhénifer Matos <jhenifer.matos@ufms.br>
2023-10-11 10:59:24 -0400

    first commit

---
+++ src/main/java/br/solutis/livepay/LivePayApplication.java
@@ -0,0 +1,13 @@
+package br.solutis.livepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/solutis/livepay/LivePayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.solutis.livepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
