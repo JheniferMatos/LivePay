d8f7312b1d375b9618350dcdf152bac0cf993a0a
caio.jesus <caionunes.profissional@gmail.com>
2023-10-28 20:29:32 -0300

    Modificações tornando esse projeto um service discovery

--- src/main/java/br/solutis/livepay/LivePayApplication.java
+++ src/main/java/br/solutis/livepay/LivePayApplication.java
@@ -2,8 +2,10 @@ package br.solutis.livepay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LivePayApplication {
 
 	public static void main(String[] args) {
